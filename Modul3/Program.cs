72b168c731ef11ebf2afbd9e80372bcda71f5067
alexandersovh <alexandrsivoh@gmail.com>
2021-01-19 22:42:47 +0300

    вывод даных

--- Modul3/Program.cs
+++ Modul3/Program.cs
@@ -6,8 +6,13 @@ namespace Modul3
     {
         static void Main(string[] args)
         {
-            string myName = "Alexandr";
-            Console.WriteLine(myName);
+            const string MyName = "Alexandr";
+            Console.WriteLine(MyName);
+            Console.WriteLine("Привет, \n мир!");
+            Console.WriteLine("\t Мне 27 лет");
+            Console.WriteLine("\t My name is \n {0}", MyName);
+            Console.WriteLine("\u0040");
+            Console.WriteLine("\x85");
             Console.ReadKey();
         }
     }
