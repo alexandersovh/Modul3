54c76b14f9e857c941dda8ea530e8caf0b0dad0f
alexandersovh <alexandrsivoh@gmail.com>
2021-01-19 22:58:40 +0300

    + Задание 3.3.2

--- Modul3/Program.cs
+++ Modul3/Program.cs
@@ -7,12 +7,18 @@ namespace Modul3
         static void Main(string[] args)
         {
             const string MyName = "Alexandr";
+            byte age = 35;
+            bool pet = true;
+            double size = 29.9;
             Console.WriteLine(MyName);
             Console.WriteLine("Привет, \n мир!");
             Console.WriteLine("\t Мне 27 лет");
             Console.WriteLine("\t My name is \n {0}", MyName);
             Console.WriteLine("\u0040");
             Console.WriteLine("\x85");
+
+            Console.WriteLine("My name is {0}\nMy age is {1}\nDo yur have a pet?{2}\nMy shoe size is {3}",MyName,age,pet,size);
+
             Console.ReadKey();
         }
     }
