6d47d9f785f8484161c2c1464825f27e1b6f9708
alexandersovh <alexandrsivoh@gmail.com>
2021-01-19 18:21:33 +0300

    Добавьте файлы проекта.

---
+++ Modul3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Modul3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Alexandr";
+            Console.WriteLine(myName);
+            Console.ReadKey();
+        }
+    }
+}
